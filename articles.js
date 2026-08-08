d3b36dede3b449552100dd0219ce525aed34db00
SevenSeven712 <2103686588@qq.com>
2026-08-08 12:39:30 +0800

    后台更新文章列表 - 2026/8/8 12:39:17

--- articles.js
+++ articles.js
@@ -1,9 +1,16 @@
 articleData = [
     {
-        "title": "标题",
+        "itle": "标题",
         "content": "正文·",
         "summary": "这啥东西？",
         "date": "2026-08-08",
+        "author": "Seven7"
+    },
+    {
+        "itle": "标题2",
+        "content": "正文2·",
+        "summary": "这啥东西？",
+        "date": "2026-08-10",
         "author": "Seven7"
     }
 ];
